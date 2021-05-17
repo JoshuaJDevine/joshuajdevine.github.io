887cae7e9f45792c7a3b5ad32c1b113e885cd358
JoshuaJDevine <jd8386@gmail.com>
2021-05-17 12:55:29 -0700

    Update

---
+++ old/main.js
@@ -0,0 +1,43 @@
+// Example Structure
+// <div className="frame">
+//     <div className="center">
+//
+//         <div className="profile">
+//             <div className="image">
+//                 <div className="circle-1"></div>
+//                 <div className="circle-2"></div>
+//                 <img src="img/Profile.PNG" width="70" height="70" alt="Jessica Potter">
+//             </div>
+//
+//             <div className="name">Jessica Potter</div>
+//             <div className="job">Visual Artist</div>
+//
+//             <div className="actions">
+//                 <button className="btn">Follow</button>
+//                 <button className="btn">Message</button>
+//             </div>
+//         </div>
+//
+//         <div className="stats">
+//             <div className="box">
+//                 <span className="value">523</span>
+//                 <span className="parameter">Posts</span>
+//             </div>
+//             <div className="box">
+//                 <span className="value">1387</span>
+//                 <span className="parameter">Likes</span>
+//             </div>
+//             <div className="box">
+//                 <span className="value">146</span>
+//                 <span className="parameter">Follower</span>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+
+let openPlayRelic = function (){
+    window.open("https://www.playrelic.com/")
+}
+let openSoundCloud = function (){
+    window.open("https://soundcloud.com/divinebladestudios")
+}
